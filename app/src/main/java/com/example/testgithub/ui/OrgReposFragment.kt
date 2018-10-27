1ab23563f63c5c8e6f2063c7cd3935b7c333edf4
Vyshakh <vyshas@gmail.com>
2018-10-27 18:28:20 +0100

    - Added swipe refresh based on network status

--- app/src/main/java/com/example/testgithub/ui/OrgReposFragment.kt
+++ app/src/main/java/com/example/testgithub/ui/OrgReposFragment.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
@@ -16,6 +17,10 @@ import android.widget.LinearLayout
 import com.example.testgithub.AppExecutors
 import com.example.testgithub.R
 import com.example.testgithub.dependencyinjection.Injectable
+import com.example.testgithub.model.OrgRepos
+import com.example.testgithub.model.Resource
+import com.example.testgithub.model.Status
+import kotlinx.android.synthetic.main.fragment_orgrepos_list.*
 import kotlinx.android.synthetic.main.fragment_orgrepos_list.view.*
 import kotlinx.android.synthetic.main.include_appbar.view.*
 import javax.inject.Inject
@@ -33,6 +38,8 @@ class OrgReposFragment : Fragment(), Injectable {
 
     @Inject
     lateinit var appExecutors: AppExecutors
+
+    private var layoutView: View? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -55,8 +62,9 @@ class OrgReposFragment : Fragment(), Injectable {
 
         view.swipeview.setOnRefreshListener {
             orgReposViewModel.refresh()
-            view.swipeview.isRefreshing = false
         }
+
+        layoutView = view
 
         return view
     }
@@ -74,14 +82,36 @@ class OrgReposFragment : Fragment(), Injectable {
     }
 
     private fun initListData() {
-        orgReposViewModel.orgReposList.observe(this, Observer {it->
-            orgReposRecyclerViewAdapter?.submitList(it?.data)
+        orgReposViewModel.orgReposList.observe(this, Observer {resource->
+            updateStatus(resource)
+            orgReposRecyclerViewAdapter?.submitList(resource?.data)
 
         })
 
 
     }
 
+    private fun updateStatus(resource: Resource<List<OrgRepos>>?) {
+        when(resource?.status){
+            Status.SUCCESS-> {
+                swipeview.isRefreshing =false
+            }
+            Status.LOADING->{
+                swipeview.isRefreshing = true
+            }
+            Status.ERROR->{
+                swipeview.isRefreshing = false
+                //show error
+                showSnackBar(resource.message)
+            }
+        }
+
+    }
+
+
+    private fun showSnackBar(message: String?) {
+        Snackbar.make(layoutView!!,message!!,Snackbar.LENGTH_LONG).show()
+    }
 
 
 }
