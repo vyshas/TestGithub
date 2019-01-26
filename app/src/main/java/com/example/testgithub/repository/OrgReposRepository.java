790c16b577978ec8643aecf9246a0e5bda2989b8
Vyshakh <vyshas@gmail.com>
2019-01-26 18:26:07 +0000

    - code cleanup

--- app/src/main/java/com/example/testgithub/repository/OrgReposRepository.java
+++ app/src/main/java/com/example/testgithub/repository/OrgReposRepository.java
@@ -81,58 +81,5 @@ public class OrgReposRepository {
         return loadRepos(organisation, true);
 
     }
-
-    /**
-     * Schedules a repeating job service.
-     */
-  /*  public void scheduleRecurringSync(Context context) {
-        Driver driver = new GooglePlayDriver(context);
-        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
-
-        // Create the Job to periodically sync Sunshine
-        Job syncSunshineJob = dispatcher.newJobBuilder()
-                                        *//* The Service that will be used to sync  data *//*
-                                        .setService(ReposService.class)
-                                        *//* Set the UNIQUE tag used to identify this Job *//*
-                                        .setTag(REPO_SYNC_TAG)
-                                        *//*
-     * Network constraints on which this Job should run. We choose to run on any
-     * network, but you can also choose to run only on un-metered networks or when the
-     * device is charging. It might be a good idea to include a preference for this,
-     * as some users may not want to download any data on their mobile plan. ($$$)
-     *//*
-                                        .setConstraints(Constraint.ON_ANY_NETWORK)
-                                        *//*
-     * setLifetime sets how long this job should persist. The options are to keep the
-     * Job "forever" or to have it die the next time the device boots up.
-     *//*
-                                        .setLifetime(Lifetime.FOREVER)
-                                        *//*
-     * We want  data to stay up to date, so we tell this Job to recur.
-     *//*
-                                        .setRecurring(true)
-                                        *//*
-     * We want the data to be synced every 3 to 4 hours. The first argument for
-     * Trigger's static executionWindow method is the start of the time frame when the
-     * sync should be performed. The second argument is the latest point in time at
-     * which the data should be synced. Please note that this end time is not
-     * guaranteed, but is more of a guideline for FirebaseJobDispatcher to go off of.
-     *//*
-                                        .setTrigger(Trigger.executionWindow(
-                                                SYNC_INTERVAL_SECONDS,
-                                                SYNC_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
-                                        *//*
-     * If a Job with the tag with provided already exists, this new job will replace
-     * the old one.
-     *//*
-                                        .setReplaceCurrent(true)
-                                        *//* Once the Job is ready, call the builder's build method to return the Job *//*
-                                        .build();
-
-        // Schedule the Job with the dispatcher
-        dispatcher.schedule(syncSunshineJob);
-
-    }*/
-
-
+    
 }
